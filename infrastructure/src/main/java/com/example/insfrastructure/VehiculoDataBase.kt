b3a541eca740006a96b39a220ccde4cd08921b1e
cristian.martinez <davick16@gmail.com>
2022-03-18 16:49:41 -0500

    implementacion DataBase

--- infrastructure/src/main/java/com/example/insfrastructure/VehiculoDataBase.kt
+++ infrastructure/src/main/java/com/example/insfrastructure/VehiculoDataBase.kt
@@ -1,8 +1,9 @@
-package com.example.domain.repository
+package com.example.insfrastructure
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.domain.entity.Vehiculo
+import com.example.domain.repository.VehiculoDao
 
 @Database(entities = [Vehiculo::class], version = 1)
 abstract class VehiculoDataBase: RoomDatabase() {
