4ca921087153eb9d5b08878ba1d33c23b2e8cbe7
cristian.martinez <davick16@gmail.com>
2022-03-19 15:52:20 -0500

    pruebas clase uno

--- domain/src/main/java/com/example/domain/entity/Carro.kt
+++ domain/src/main/java/com/example/domain/entity/Carro.kt
@@ -2,6 +2,7 @@ package com.example.domain.entity
 
 import androidx.room.PrimaryKey
 
-class Carro(@PrimaryKey override var numeroPlaca: String) : Vehiculo() {
+class Carro( numeroPlaca: String) : Vehiculo() {
+    @PrimaryKey override var numeroPlaca = numeroPlaca.uppercase()
 
 }
---
+++ domain/src/test/java/com/example/domain/entity/CarroTest.kt
@@ -0,0 +1,36 @@
+package com.example.domain.entity
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class CarroTest {
+
+    @Test
+    fun obtenerNumeroPlaca_NumeroPlacaCorrecto_NumeroPlacaNoVacio() {
+        var carroTestNumeroPlaca :Carro = Carro("HSU531")
+        var numeroPlaca =carroTestNumeroPlaca.numeroPlaca
+
+        assert(numeroPlaca.isNotEmpty())
+    }
+
+    @Test
+    fun obtenerNumeroPlacaMayusculas_NumeroPlacaCorrectoMayusculas_NumeroPlacaMayusculas() {
+        var carroTestNumeroPlaca :Carro = Carro("HSU531")
+        var numeroPlaca =carroTestNumeroPlaca.numeroPlaca
+
+        assert(numeroPlaca.equals(numeroPlaca.uppercase()))
+    }
+
+    @Test
+    fun obtenerNumeroPlacaMayusculas_NumeroPlacaCorrectoMinusculas_NumeroPlacaMayusculas() {
+        var carroTestNumeroPlaca :Carro = Carro("hsu531")
+        var numeroPlaca =carroTestNumeroPlaca.numeroPlaca
+
+        assert(numeroPlaca.equals(numeroPlaca.uppercase()))
+    }
+
+
+}
