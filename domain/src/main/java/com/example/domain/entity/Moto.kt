a82e1dfa9f8e48796ad6c9590d2612d20031cba0
cristian.martinez <davick16@gmail.com>
2022-03-22 15:03:36 -0500

    cambios en definicion de @entity

--- domain/src/main/java/com/example/domain/entity/Moto.kt
+++ domain/src/main/java/com/example/domain/entity/Moto.kt
@@ -1,8 +1,9 @@
 package com.example.domain.entity
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
+@Entity
 class Moto( numeroPlaca: String, var cilindrajeAlto: Boolean = false) : Vehiculo() {
 
     @PrimaryKey override var numeroPlaca = numeroPlaca.uppercase()
--- domain/src/main/java/com/example/domain/entity/Vehiculo.kt
+++ domain/src/main/java/com/example/domain/entity/Vehiculo.kt
@@ -1,9 +1,5 @@
 package com.example.domain.entity
 
-import androidx.room.Entity
-
-
-@Entity
 abstract class Vehiculo() {
    abstract val numeroPlaca: String
 
