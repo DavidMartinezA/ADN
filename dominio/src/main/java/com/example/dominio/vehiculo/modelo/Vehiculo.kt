dd13913ad0fc53bc508ea5cac207ac19435e0d13
cristian.martinez <davick16@gmail.com>
2022-03-25 15:26:17 -0500

    validaciones

--- dominio/src/main/java/com/example/dominio/vehiculo/modelo/Vehiculo.kt
+++ dominio/src/main/java/com/example/dominio/vehiculo/modelo/Vehiculo.kt
@@ -7,12 +7,15 @@ abstract class Vehiculo(val placaVehiculo: String) {
         const val FORMATO_PLACA = "[a-zA-Z]{3}-?[0-9]{2}[a-zA-Z0-9]?"
     }
 
-    val mensajeNoEstaAutorizado = "No Esta Autorizado Ingresar."
-    lateinit var campoVacio: String
-
     init {
-        if (placaVehiculo.isEmpty() || !placaVehiculo.matches(Regex(FORMATO_PLACA))) {
-            campoVacio = mensajeNoEstaAutorizado
+
+        var validado = false
+        fun validacion(): Boolean {
+            if (placaVehiculo.isNotEmpty() || placaVehiculo.matches(Regex(FORMATO_PLACA))) {
+                validado = true
+            }
+            return validado
         }
     }
+
 }
